430354c2325db9a353756045471974ed439e5f3b
DK <laancer4@gmail.com>
2024-03-29 21:02:20 +0900

    ~12 완벽공략

--- src/main/java/me/whiteship/chapter01/item03/functionalinterface/DefaultFunctions.java
+++ src/main/java/me/whiteship/chapter01/item03/functionalinterface/DefaultFunctions.java
@@ -4,6 +4,7 @@ package me.whiteship.chapter01.item03.functionalinterface;
 import me.whiteship.chapter01.item03.methodreference.Person;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -12,7 +13,10 @@ import java.util.function.Supplier;
 public class DefaultFunctions {
 
     public static void main(String[] args) {
+        int a = 1;
         Function<Integer, String> intToString = Object::toString;
+
+        System.out.println(intToString);
 
         Supplier<Person> personSupplier = Person::new;
         Function<LocalDate, Person> personFunction = Person::new;
